e35f7438bced9b9fe877238cd78c72b1c15d46c1
Matheus Gomes <matheusnascgomes@gmail.com>
2018-06-25 23:20:53 -0300

    First commit

---
+++ src/App.js
@@ -0,0 +1,19 @@
+import React from 'react';
+
+import './styles/global';
+
+import Sidebar from './components/Sidebar';
+import Player from './components/Player';
+
+import { Wrapper, Container } from './styles/components';
+
+const App = () => (
+  <Wrapper>
+    <Container>
+      <Sidebar />
+    </Container>
+    <Player />
+  </Wrapper>
+);
+
+export default App;
